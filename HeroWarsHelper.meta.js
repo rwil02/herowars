88658acbc31672c50e1916d3fd9f7be42949c5e6
Roger Willcocks <RogerW@l-space-design.com>
2025-07-21 18:50:46 +1200

    Realign to match TM/GM update model.

---
+++ HeroWarsHelper.meta.js
@@ -0,0 +1,17 @@
+﻿// ==UserScript==
+// @name         Hero Wars Helper
+// @namespace    http://l-space-design.com/
+// @version      1.0.3
+// @description  Get Hero Data for Hero Wars
+// @author       Roger Willcocks
+// @match        https://*.hero-wars.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hero-wars.com
+// @require      http://code.jquery.com/jquery-3.7.1.min.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @note         Requires Tampermonkey 4.19+ for dynamic menu support
+// @downloadURL  https://raw.githubusercontent.com/rwil02/herowars/main/HeroWarsHelper.user.js
+// ==/UserScript==
--- README.md
+++ README.md
@@ -4,12 +4,14 @@
 Roger Willcocks
 
 ### Version
-0.5
+1.0.3
 
 ## Links
 **Game:**  https://www.hero-wars.com/
 
 **Repo:** https://github.com/rwil02/herowars
+
+**Installation:** https://raw.githubusercontent.com/rwil02/herowars/main/HeroWarsHelper.user.js
 
 **TamperMonkey:** https://chrome.google.com/webstore/search/tampermonkey
 
@@ -21,6 +23,8 @@ Roger Willcocks
 5. Highlight which enemy heros are different within a team from when last fought
 6. Limit history to last 500 battles for each of Arena and Grand Arena
 7. Display last 5 battles only for each opponent
+8. Export and import history to/from local storage
+9. Localization support for English and German languages
 
 ## Screenshot
 ![Screenshot](screenshot.png)
